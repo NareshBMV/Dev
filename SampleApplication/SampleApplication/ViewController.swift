d8e1fe968756a52589123f9538d544dbb9bcefa7
Naresh <inareshbabumv@gmail.com>
2023-06-27 10:34:18 +0530

    Dev Branch initial commit

---
+++ SampleApplication/SampleApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApplication
+//
+//  Created by Naresh on 27/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Dev Branch")
+    }
+
+
+}
+
